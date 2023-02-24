b66ec4802e31b881e55bbfe4ff9a60509301ad79
pedro <pedro@TORRE-RYZEN5>
2023-02-24 18:40:45 +0100

    Valor=1

---
+++ src/pedro/git/PedroGIT.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pedro.git;
+
+/**
+ *
+ * @author pedro
+ */
+public class PedroGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        int valor;
+        
+        valor = 1;
+        
+        System.out.println ("valor " + valor);
+        
+    
+    }
+    
+}
