555daa394d9fd235e57520b536db09e067342ffb
pedro <pedro@TORRE-RYZEN5>
2023-02-26 10:42:16 +0100

    elegir valor no es menor sin comentario en main

--- src/pedro/git/PedroGIT.java
+++ src/pedro/git/PedroGIT.java
@@ -14,7 +14,7 @@ public class PedroGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO 
     Scanner sc =new Scanner (System.in);
        int valor;
        int valorMenor;
